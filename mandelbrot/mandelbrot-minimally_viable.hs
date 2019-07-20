6dcea05087b5c5cb3ee7ae8cdc238165f1e33c9f
work <work@admins-MacBook-Pro.local>
2019-07-20 21:25:53 +0200

    tweaks

--- mandelbrot/mandelbrot-minimally_viable.hs
+++ mandelbrot/mandelbrot-minimally_viable.hs
@@ -7,9 +7,10 @@ mandelField tl br z     = [ [ isMandelbrot (r :+ i) | r <- [startR, startR+step 
                  endR   = startR + realPart br - realPart tl
                  step   = 1.0 / z
 printMandelbrot tl br z = putStrLn $ unlines . map (unwords . map (\c -> if c then " #" else "  ")) $ mandelField tl br z
-main = printMandelbrot ((-1.5) :+ ( 1.5)) (( 1.5) :+ (-1.5)) 20.0
+main = printMandelbrot ((-1.5) :+ ( 1.5)) (( 1.5) :+ (-1.5)) 20
 
 -- 10 LINES!!!
 -- And even though I inlined the one-time variables and the mandelSeq code, as
 -- well as the mandelFieldToString function this is still a readable piece of
 -- code. Bloody awesome!
+-- Note: 180 is the largest working zoom for iTerm
